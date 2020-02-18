38d2504baa50cb0dce6d75ef5e65e134c519fad2
wonderit <wonsuk@broadinstitute.org>
2020-02-17 22:31:24 -0500

    boost test file added

---
+++ mpc/code/boost_test.cpp
@@ -0,0 +1,35 @@
+//#ifndef __PROTOCOL_H_
+//#define __PROTOCOL_H_
+
+//#include "mpc.h"
+//#include "util.h"
+//#include "protocol.h"
+//#include <vector>
+//#include <NTL/mat_ZZ_p.h>
+//#include <NTL/mat_ZZ.h>
+//#include <NTL/ZZ.h>
+//#include <NTL/BasicThreadPool.h>
+#include <iostream>
+//#include <fstream>
+//#include <sstream>
+
+//using namespace NTL;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+//  Vec<ZZ_p> a, b, c;
+//  Init(a, 543653);
+//  ZZ_p fp_one = DoubleToFP(1, Param::NBIT_K, Param::NBIT_F);
+//  AddScalar(a, fp_one);
+//  tic();
+//  b = 2 * a;
+//  mpc.FPDiv(c, a, b);
+//  toc();
+
+  __int128_t a = 1;
+
+  cout << a << " " << endl;
+  return true;
+}
