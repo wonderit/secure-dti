84e2f7b396dc0e538b4ace0907f976903c362730
wonderit <wonsuk@broadinstitute.org>
2020-02-18 16:01:45 -0500

    cmakelist editted for debugging on Clion

--- mpc/code/boost_test.cpp
+++ mpc/code/boost_test.cpp
@@ -1,19 +1,16 @@
-//#ifndef __PROTOCOL_H_
-//#define __PROTOCOL_H_
-
-//#include "mpc.h"
-//#include "util.h"
-//#include "protocol.h"
-//#include <vector>
-//#include <NTL/mat_ZZ_p.h>
-//#include <NTL/mat_ZZ.h>
-//#include <NTL/ZZ.h>
-//#include <NTL/BasicThreadPool.h>
+#include "mpc.h"
+#include "util.h"
+#include "protocol.h"
+#include <vector>
+#include <NTL/mat_ZZ_p.h>
+#include <NTL/mat_ZZ.h>
+#include <NTL/ZZ.h>
+#include <NTL/BasicThreadPool.h>
 #include <iostream>
-//#include <fstream>
-//#include <sstream>
+#include <fstream>
+#include <sstream>
 
-//using namespace NTL;
+using namespace NTL;
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -22,14 +19,31 @@ int main(int argc, char* argv[])
 //  Vec<ZZ_p> a, b, c;
 //  Init(a, 543653);
 //  ZZ_p fp_one = DoubleToFP(1, Param::NBIT_K, Param::NBIT_F);
-//  AddScalar(a, fp_one);
 //  tic();
-//  b = 2 * a;
-//  mpc.FPDiv(c, a, b);
 //  toc();
 
-  __int128_t a = 1;
+  int as = 1;
 
-  cout << a << " " << endl;
+  cout << as << " sss " << endl;
+  cout <<  Param::NBIT_K <<  Param::NBIT_F << " sss " << endl;
+
+
+  vector< pair<int, int> > pairs;
+  pairs.push_back(make_pair(0, 1));
+  pairs.push_back(make_pair(0, 2));
+  pairs.push_back(make_pair(1, 2));
+
+  /* Initialize MPC environment */
+  MPCEnv mpc;
+  if (!mpc.Initialize(1, pairs)) {
+    tcout() << "MPC environment initialization failed" << endl;
+    return 1;
+  }
+
+  tcout() << "[Fixed-point ZZ_p <-> Double conversion] ";
+  ZZ_p x = DoubleToFP(3.141592653589793238462643383279, Param::NBIT_K, Param::NBIT_F);
+  double d = ABS(FPToDouble(x, Param::NBIT_K, Param::NBIT_F) - 3.141592653589793238462643383279);
+  tcout() << d << endl;
+
   return true;
 }
